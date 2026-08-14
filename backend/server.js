576074995d1bd444300cf6bdf1800c36869c95c2
vaishali19-ui <vaishalisuryan19@gmail.com>
2026-08-14 10:21:17 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -15,4 +15,7 @@ mongoose.connect(process.env.MONGO_URI)
 const PORT = process.env.PORT || 5000;
 const subjectRoutes = require('./routes/subjects');
 app.use('/api/subjects', subjectRoutes);
+app.get('/', (req, res) => {
+  res.send('StudySync backend is running');
+});
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
